30aec483526fd452068497693e51f8917eaaf532
KetoAcc2 <82528000+KetoAcc2@users.noreply.github.com>
2021-04-19 00:50:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ import hashlib
 
 app = FastAPI()
 
-# /auth?password=haslo&password_hash=013c6889f799cd986a735118e1888727d1435f7f623d05d58c61bf2cd8b49ac90105e5786ceaabd62bbc27336153d0d316b2d13b36804080c44aa6198c533215
-# /auth?password=haslo&password_hash=f34ad4b3ae1e2cf33092e2abb60dc0444781c15d0e2e9ecdb37e4b14176a0164027b05900e09fa0f61a1882e0b89fbfa5dcfcc9765dd2ca4377e2c794837e091
-
 
 @app.get("/auth")
-def root(password: str, password_hash: str, response: Response):
+def validate_password(password: str, password_hash: str, response: Response):
+
     normal_to_hashed = hashlib.sha512(password.encode()).hexdigest()
     response.status_code = status.HTTP_401_UNAUTHORIZED
     if password_hash == normal_to_hashed:
@@ -18,5 +16,5 @@ def root(password: str, password_hash: str, response: Response):
     return response
 
 
-if __name__ == "__main__":
+if name == 'main':
     uvicorn.run(app)
