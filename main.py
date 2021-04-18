75adc96f64c7b21656183a3a838c2d5f0169b8e7
KetoAcc2 <82528000+KetoAcc2@users.noreply.github.com>
2021-04-19 01:49:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status, Response
+from fastapi import FastAPI, status, Response, Request
 import uvicorn
 import hashlib
 
@@ -6,11 +6,34 @@ app = FastAPI()
 
 
 @app.get("/auth")
-def validate_password(password: str, password_hash: str, response: Response):
-    normal_to_hashed = hashlib.sha512(password.encode()).hexdigest()
-    response.status_code = status.HTTP_401_UNAUTHORIZED
-    if password_hash == normal_to_hashed:
-        response.status_code = status.HTTP_204_NO_CONTENT
+def validate_password(request: Request, response: Response):
+    print(str(request.query_params))
+
+    params = str(request.query_params).split('&')
+    password = ''
+    turner = False
+    for i in params[0]:
+        if turner:
+            password += i
+        if i == '=':
+            turner = True
+
+    password_hash = ''
+    turner = False
+    for i in params[1]:
+        if turner:
+            password_hash += i
+        if i == '=':
+            turner = True
+
+    if password_hash is not None and password is not None and password_hash != '' and password != '':
+        normal_to_hashed = hashlib.sha512(password.encode()).hexdigest()
+        if password_hash == normal_to_hashed:
+            response.status_code = status.HTTP_204_NO_CONTENT
+        else:
+            response.status_code = status.HTTP_401_UNAUTHORIZED
+    else:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
 
     return response
 
