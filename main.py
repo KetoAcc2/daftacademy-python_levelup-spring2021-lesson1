61d35d4eff785f8026788b40167d2d8e1fca8e7a
KetoAcc2 <82528000+KetoAcc2@users.noreply.github.com>
2021-04-19 00:28:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
+from fastapi import FastAPI, status, Response
 import uvicorn
-from fastapi import FastAPI
+import hashlib
 
 app = FastAPI()
 
-
-@app.get("/method", status_code=200)
-def test_counter():
-    return {"method": "GET"}
+# /auth?password=haslo&password_hash=013c6889f799cd986a735118e1888727d1435f7f623d05d58c61bf2cd8b49ac90105e5786ceaabd62bbc27336153d0d316b2d13b36804080c44aa6198c533215
+# /auth?password=haslo&password_hash=f34ad4b3ae1e2cf33092e2abb60dc0444781c15d0e2e9ecdb37e4b14176a0164027b05900e09fa0f61a1882e0b89fbfa5dcfcc9765dd2ca4377e2c794837e091
 
 
-@app.put("/method", status_code=200)
-def test_counter():
-    return {"method": "PUT"}
-
-
-@app.options("/method", status_code=200)
-def test_counter():
-    return {"method": "OPTIONS"}
-
-
-@app.delete("/method", status_code=200)
-def test_counter():
-    return {"method": "DELETE"}
-
-
-@app.post("/method", status_code=201)
-def test_counter():
-    return {"method": "POST"}
+@app.get("/auth")
+def root(password: str, password_hash: str, response: Response):
+    if password_hash == hashlib.sha512(password.encode()).hexdigest():
+        response.status_code = status.HTTP_204_NO_CONTENT
+        return None
+    else:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return None
 
 
 if __name__ == "__main__":
