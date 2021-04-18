cfbfd5b7af3015401b362ad8eedf2c1dfeb81f01
KetoAcc2 <82528000+KetoAcc2@users.noreply.github.com>
2021-04-19 01:56:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,30 +10,34 @@ def validate_password(request: Request, response: Response):
     print(str(request.query_params))
 
     params = str(request.query_params).split('&')
-    password = ''
-    turner = False
-    for i in params[0]:
-        if turner:
-            password += i
-        if i == '=':
-            turner = True
+    if len(params) == 2:
+        password = ''
+        turner = False
+        for i in params[0]:
+            if turner:
+                password += i
+            if i == '=':
+                turner = True
 
-    password_hash = ''
-    turner = False
-    for i in params[1]:
-        if turner:
-            password_hash += i
-        if i == '=':
-            turner = True
+        password_hash = ''
+        turner = False
+        for i in params[1]:
+            if turner:
+                password_hash += i
+            if i == '=':
+                turner = True
 
-    if password_hash is not None and password is not None and password_hash != '' and password != '':
-        normal_to_hashed = hashlib.sha512(password.encode()).hexdigest()
-        if password_hash == normal_to_hashed:
-            response.status_code = status.HTTP_204_NO_CONTENT
+        if password_hash is not None and password is not None and password_hash != '' and password != '':
+            normal_to_hashed = hashlib.sha512(password.encode()).hexdigest()
+            if password_hash == normal_to_hashed:
+                response.status_code = status.HTTP_204_NO_CONTENT
+            else:
+                response.status_code = status.HTTP_401_UNAUTHORIZED
         else:
             response.status_code = status.HTTP_401_UNAUTHORIZED
+
     else:
-        response.status_code = status.HTTP_401_UNAUTHORIZED
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
 
     return response
 
