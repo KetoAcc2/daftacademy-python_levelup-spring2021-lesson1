8b8b4ec407ee61ce571f5ad8fc5c27d6965537e9
KetoAcc2 <82528000+KetoAcc2@users.noreply.github.com>
2021-04-19 18:16:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,11 +22,50 @@ class Data(BaseModel):
     surname: str
 
 
+class Patient:
+    def __init__(self, patient_id, name, surname, register_date, vaccination_date):
+        self.patient_id = patient_id
+        self.name = name
+        self.surname = surname
+        self.register_date = register_date
+        self.vaccination_date = vaccination_date
+
+
+@app.get("/patient/{patient_id}")
+def get_patient_by_id(patient_id: int, response: Response):
+    response.status_code = 200
+    if patient_id < 1:
+        response.status_code = 400
+        return response
+    else:
+        ids = list()
+        for i in patients:
+            ids.append(i.patient_id)
+        if patient_id not in ids:
+            response.status_code = 404
+            return response
+
+    patient = patients[patient_id + 1]
+    preparedData = {
+        "id": patient.patient_id,
+        "name": patient.name,
+        "surname": patient.surname,
+        "register_date": patient.register_date,
+        "vaccination_date": patient.vaccination_date
+    }
+    return preparedData
+
+
 counter = ClientIdCounter()
+tmpList = list()
+tmpList.append(Patient(0, '', '', '', ''))
+patients = tmpList
+patients.clear()
 
 
 @app.post("/register")
 def register_vaccination(data: Data, response: Response):
+    response.status_code = status.HTTP_201_CREATED
     if data.name is None or data.name == '' or\
             data.surname is None or data.surname == '':
         response.status_code = status.HTTP_401_UNAUTHORIZED
@@ -46,7 +85,7 @@ def register_vaccination(data: Data, response: Response):
     print(register_date)
     print(vaccination_date)
 
-    response.status_code = status.HTTP_201_CREATED
+    patients.append(Patient(client_id, data.name, data.surname, register_date, vaccination_date))
 
     preparedData = {
         "id": client_id,
